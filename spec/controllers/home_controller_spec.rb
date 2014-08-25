aca16f9997f3c6f1bc066a8005e8ce7f57ab8f78
Agnieszka Malaszkiewicz <womanonrails@users.noreply.github.com>
2014-08-25 13:36:08 +0200

    Contact form worked

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe 'GET index' do
-    it 'assigns all homes as @homes' do
+    it 'render home template' do
       get :index
       expect(response).to render_template(:index)
+    end
+
+    it 'has ContactForm object' do
+      get :index
+      expect(assigns(:contact)).to be_an(ContactForm)
     end
   end
 end
--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'home/index', type: :view do
+  before { assign(:contact, ContactForm.new) }
+
   it 'renders home' do
     render
   end
