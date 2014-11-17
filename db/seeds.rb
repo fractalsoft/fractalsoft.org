729da88fa4dadf7688e8850f71108a0e46a39b02
Agnieszka Malaszkiewicz <womanonrails@users.noreply.github.com>
2014-11-17 15:11:24 +0100

    Project show images load from data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 YAML.load_file('db/projects.yml')['projects'].each do |array|
   image = File.open(Rails.root + 'db' + array['thumbnail'])
-  Project.create(
+  project = Project.create(
     title: array['title'],
     subtitle: array['subtitle'],
     description: array['description'],
@@ -8,4 +8,9 @@ YAML.load_file('db/projects.yml')['projects'].each do |array|
     thumbnail: image,
     year: array['year']
   )
+
+  array['images'].each do |hash|
+    file = File.open(Rails.root + 'db' + hash['image'])
+    image = Image.create(image: file, kind: hash['kind'], project: project)
+  end
 end
