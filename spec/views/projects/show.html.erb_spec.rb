f156f15121d0be29c1a182d52fffa749343abb9b
Agnieszka Malaszkiewicz <womanonrails@users.noreply.github.com>
2014-11-17 13:56:47 +0100

    Fixed tests

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "projects/show", :type => :view do
+RSpec.describe 'projects/show', type: :view do
   before(:each) do
     @project = assign(:project, Project.create!(
-      :title => "Title",
-      :subtitle => "Subtitle",
-      :descritpion => "MyText",
-      :url => "Url",
-      :thumbnail => "Thumbnail"
+      title: 'Title',
+      subtitle: 'Subtitle',
+      description: 'MyText',
+      url: 'Url',
+      thumbnail: 'Thumbnail'
     ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in view' do
     render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Subtitle/)
     expect(rendered).to match(/MyText/)
     expect(rendered).to match(/Url/)
-    expect(rendered).to match(/Thumbnail/)
   end
 end
