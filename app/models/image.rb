0fbea27cb83d6a27e72bcdeeab6e9c5c5a113af9
Agnieszka Matysek <amatysek@fractalsoft.org>
2014-11-17 15:11:24 +0100

    Project show images load from data base

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,22 @@
+# Image class
 class Image < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
+
   belongs_to :project
+
+  def self.logo
+    where(kind: 'logo').first
+  end
+
+  def self.pages
+    where(kind: 'page')
+  end
+
+  def self.pc
+    where(kind: 'pc').first
+  end
+
+  def self.top
+    where(kind: 'top').first
+  end
 end
