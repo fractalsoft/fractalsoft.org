ecad691a0953b76d9f2ef1d7cb23f2a5e539dfd4
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-07-15 15:51:40 +0200

    Detection or manual locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = session[:locale] || detect_locale
+  end
+
+  def detect_locale
+    locales = I18n.available_locales
+    http_accept_language.compatible_language_from(locales)
+  end
 end
