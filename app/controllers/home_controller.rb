4f681d15605bdb7357b87e819aae7d89ad982c82
Alek Malaszkiewicz <alek@fractalsoft.org>
2015-07-16 10:42:59 +0200

    Caching homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 # Home controller
 class HomeController < ApplicationController
+  caches_page :index
+  before_filter(only: [:index]) { @page_caching = true }
+
   def index
     @contact_form = ContactForm.new
     @people = Person.all
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 # Sessions controller to change language
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:update]
+
   def update
+    expire_page root_path
     session[:locale] = params[:locale]
     redirect_to :back
   end
