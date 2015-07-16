88c211236c403690d98ce6700cbd8517fdde2cf1
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-07-16 14:07:13 +0200

    Fix locale path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # resources :people, path: 'team', only: [:index]
   # resources :projects, only: [:index, :show]
-  put 'locale/:locale', to: 'sessions#update'
+  put 'locale/:locale', to: 'sessions#update', as: 'locale'
   resources :contact_forms, path: 'contact-forms', only: [:new, :create]
   resource :sessions, only: [:update]
   root 'home#index'
