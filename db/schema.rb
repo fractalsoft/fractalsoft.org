ebc1421a3f181d5d795fd136e1d159931bd1a0d3
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-07-29 15:45:52 +0200

    Create simple team view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125113017) do
+ActiveRecord::Schema.define(version: 20150729133644) do
 
   create_table "images", force: :cascade do |t|
     t.string   "image"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141125113017) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "blog"
   end
 
   create_table "projects", force: :cascade do |t|
