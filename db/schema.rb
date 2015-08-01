79cc34f86d02e35d77972d8ff7aa03645f953cc9
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-08-01 10:23:32 +0200

    Command 'rails g migration add_slug_to_people slug'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730121912) do
+ActiveRecord::Schema.define(version: 20150801082145) do
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "images", force: :cascade do |t|
     t.string   "image"
@@ -37,7 +50,10 @@ ActiveRecord::Schema.define(version: 20150730121912) do
     t.datetime "updated_at"
     t.string   "blog"
     t.text     "introduction"
+    t.string   "slug"
   end
+
+  add_index "people", ["slug"], name: "index_people_on_slug"
 
   create_table "person_translations", force: :cascade do |t|
     t.integer  "person_id",    null: false
