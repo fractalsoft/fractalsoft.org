96b7d6b32c0298e74bc965027a788e6c2c7c386e
Agnieszka Matysek <amatysek@fractalsoft.org>
2015-08-28 12:27:23 +0200

    Add uuid for person to contibutions table

--- db/migrate/20150828073221_create_contributions.rb
+++ db/migrate/20150828073221_create_contributions.rb
@@ -2,7 +2,7 @@ class CreateContributions < ActiveRecord::Migration
   def change
     create_table :contributions do |t|
       t.references :project, index: true, foreign_key: true
-      t.references :person, index: true, foreign_key: true
+      t.uuid :person, index: true, foreign_key: true
       t.string :name
 
       t.timestamps null: false
