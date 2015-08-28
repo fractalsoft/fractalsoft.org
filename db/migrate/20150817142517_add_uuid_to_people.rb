db445bd4ab3c304503adba20acf0957a7a6dcad5
Alek Malaszkiewicz <alek@fractalsoft.org>
2015-08-28 10:49:12 +0200

    Change sqlite to postgres

--- db/migrate/20150817142517_add_uuid_to_people.rb
+++ db/migrate/20150817142517_add_uuid_to_people.rb
@@ -1,5 +1,13 @@
 class AddUuidToPeople < ActiveRecord::Migration
   def change
-    add_column :people, :uuid, :uuid, default: 'uuid_generate_v4()'
+    add_column :people, :uuid, :uuid,
+               default: 'uuid_generate_v4()', null: false
+
+    change_table :people do |t|
+      t.remove :id
+      t.rename :uuid, :id
+    end
+
+    execute 'ALTER TABLE people ADD PRIMARY KEY (id);'
   end
 end
