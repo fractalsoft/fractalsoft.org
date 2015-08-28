732a32420170ca1fcd1005bd164af2921718c983
Agnieszka Malaszkiewicz <womanonrails@users.noreply.github.com>
2015-08-28 09:50:37 +0200

    Add contibution table with translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,19 @@ YAML.load_file('db/projects.yml')['projects'].each do |hash|
     end
   end
 end
+
+YAML.load_file('db/contributions.yml')['contributions'].each do |hash|
+  params = hash.symbolize_keys
+  project = Project.find_by(title: params.delete(:project_title))
+  person = Person.find_by(fullname: params.delete(:person_fullname))
+
+  params[:list_name].each do |translations|
+    contribution = Contribution.create(project: project, person: person)
+
+    translations.symbolize_keys.each do |locale, text|
+      I18n.locale = locale
+      contribution.name = text
+      contribution.save
+    end
+  end
+end
