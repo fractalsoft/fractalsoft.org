4494b7367d95152f2504d30071beb7fcf014435b
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-09-16 15:07:10 +0200

    Fix routes & tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
   describe 'GET index' do
     it 'render home template' do
-      get :index
+      get :index, locale: :en
       expect(response).to render_template(:index)
     end
 
-    it 'has ContactForm object' do
-      get :index
+    it 'has HomeFacade object' do
+      get :index, locale: :en
       expect(assigns(:home)).to be_an(HomeFacade)
     end
   end
--- spec/routing/home_routing_spec.rb
+++ spec/routing/home_routing_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :routing do
   describe 'routing' do
-    it 'routes to #index' do
-      expect(get('/')).to route_to('home#index')
+    I18n.available_locales.each do |locale|
+      it 'routes to #index' do
+        path = "/#{locale}"
+        expect(get(path)).to route_to('home#index', locale: locale.to_s)
+      end
     end
   end
 end
