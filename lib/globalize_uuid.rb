e656ea73d512003aca8e7d678d3310c18e0a5282
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-09-16 15:07:10 +0200

    Fix routes & tests

--- lib/globalize_uuid.rb
+++ lib/globalize_uuid.rb
@@ -9,10 +9,10 @@ module Globalize
 
         # rubocop:disable all
         def create_translation_table
-          connection.create_table(translations_table_name, id: primary_key_type) do |t|
-            t.references table_name.sub(/^#{table_name_prefix}/, '').singularize, null: false, type: primary_key_type
-            t.string :locale, null: false
-            t.timestamps null: false
+          connection.create_table(translations_table_name, id: primary_key_type) do |table|
+            table.references table_name.sub(/^#{table_name_prefix}/, '').singularize, null: false, type: primary_key_type
+            table.string :locale, null: false
+            table.timestamps null: false
           end
         end
         # rubocop:enable all
