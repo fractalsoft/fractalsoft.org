ddb270bef82d1df9b4c04018dd888ecf3e1da791
Alex Malaszkiewicz <alek@fractalsoft.org>
2016-08-26 10:22:43 +0200

    Create protection against wrong format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_locale
+  rescue_from ActionController::UnknownFormat, with: :raise_not_found
 
   private
+
+  def raise_not_found
+    render(text: 'Not Found', status: 404)
+  end
 
   def set_locale
     I18n.locale = params[:locale] if params[:locale]
