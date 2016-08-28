007e2718cf83539d4c6a060df28706826393b373
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2016-08-28 18:37:23 +0200

    Clean up

--- app/models/concerns/create_or_update_by_keys_extension.rb
+++ app/models/concerns/create_or_update_by_keys_extension.rb
@@ -1,11 +1,22 @@
 # Extension to update exist objects or create them
 module CreateOrUpdateByKeysExtension
   def create_or_update_by_keys(keys, params)
-    object = nil
-    keys.each do |key|
-      object = find_by("#{key} = ?", params[key]) if params[key].present?
-      break if object
-    end
+    object = find_by_keys(keys, params)
     object ? object.update(params) && object : create(params)
+  end
+
+  private
+
+  def find_by_key(key, params)
+    value = params[key]
+    value.presence && find_by("#{key} = ?", value)
+  end
+
+  def find_by_keys(keys, params)
+    keys.each_with_object([]) do |key|
+      object = find_by_key(key, params)
+      return object if object
+    end
+    nil
   end
 end
