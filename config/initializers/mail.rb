5ea5f21fb6ad9d886bebcd47ff25d060bed48f40
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2017-01-06 12:52:36 +0100

    Remove letter_opener, enable Mandrill only in production

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,14 @@
-ActionMailer::Base.smtp_settings = {
-  address:   'smtp.mandrillapp.com',
-  port:      587,
-  user_name: Rails.application.secrets.mandrill_username,
-  password:  Rails.application.secrets.mandrill_api_key,
-  domain:    'fractalsoft.org'
-}
-ActionMailer::Base.delivery_method = :smtp
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    address:   'smtp.mandrillapp.com',
+    port:      587,
+    user_name: Rails.application.secrets.mandrill_username,
+    password:  Rails.application.secrets.mandrill_api_key,
+    domain:    'fractalsoft.org'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
-MandrillMailer.configure do |config|
-  config.api_key =  Rails.application.secrets.mandrill_api_key
+  MandrillMailer.configure do |config|
+    config.api_key = Rails.application.secrets.mandrill_api_key
+  end
 end
