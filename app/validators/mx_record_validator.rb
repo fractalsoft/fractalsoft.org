b5f9f0d8131dba22e880cba15de8341358bddf62
Alex Malaszkiewicz <alek@fractalsoft.org>
2017-01-07 16:37:49 +0100

    Move logic with mx record detection to separate class

--- app/validators/mx_record_validator.rb
+++ app/validators/mx_record_validator.rb
@@ -2,23 +2,33 @@ require 'resolv'
 
 # https://en.wikipedia.org/wiki/MX_record
 class MxRecordValidator < ActiveModel::EachValidator
-  attr_reader :email
-
   def validate_each(record, attribute, email)
-    @email = email
+    mail_servers = MailServersFromEmail.new(email)
     record.errors.add(attribute, :fake_email) if mail_servers.empty?
   end
 
-  private
+  class MailServersFromEmail
+    attr_reader :email
 
-  def domain_name
-    email.split('@').last
-  end
+    def initialize(email)
+      @email = email
+    end
 
-  def mail_servers
-    Resolv::DNS.open.getresources(
-      domain_name,
-      Resolv::DNS::Resource::IN::MX
-    )
+    def empty?
+      mail_servers.empty?
+    end
+
+    private
+
+    def domain_name
+      email.split('@').last
+    end
+
+    def mail_servers
+      Resolv::DNS.open.getresources(
+        domain_name,
+        Resolv::DNS::Resource::IN::MX
+      )
+    end
   end
 end
