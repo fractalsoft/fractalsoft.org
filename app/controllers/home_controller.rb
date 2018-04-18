dee7fb7a8d01de8d3e49651064174c812c40c257
Alex Malaszkiewicz <alek@fractalsoft.org>
2018-04-18 12:57:51 +0200

    Remove commmenting code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
-  # caches_page :index
-  # before_action(only: [:index]) { @page_caching = true }
-
   def index
     @home = HomeFacade.new
-    # expires_in 30.minutes, public: true
   end
 
   private
