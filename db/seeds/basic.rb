34cbff0fc6e79ea1fd7cd844c1c7b4253a829099
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2019-02-27 20:24:09 +0100

    Chore/split seeds (#77)
    * Split seeds file into separate files in different folders
    
    * Split price list from json

---
+++ db/seeds/basic.rb
@@ -0,0 +1,25 @@
+require 'oj'
+
+json_file = File.join(Rails.root, 'db', 'seeds', 'basic', 'data.json')
+hash = Oj.load(File.read(json_file), symbol_keys: true)
+
+hash[:projects].each do |params|
+  Project.create_or_update_by_keys([:slug], params)
+end
+
+hash[:team].each do |params|
+  contributions = params.delete(:contributions) || []
+  person = Person.create_or_update_by_keys(
+    [:nickname, :fullname],
+    params
+  )
+  contributions.each do |project_params|
+    slug = project_params.delete(:slug)
+    project = Project.find_by(slug: slug)
+    project_params[:names].each do |name|
+      item = name[:name] || name[:name_en]
+      options = { person_id: person.id, project_id: project.id, name: item }
+      Contribution.where(options).first_or_create(name)
+    end
+  end
+end
