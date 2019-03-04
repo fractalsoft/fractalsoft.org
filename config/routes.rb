43982799e14bb5fb98c3c4994a6556b7bd25669c
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2019-03-04 13:18:14 +0100

    Translate routes with route_translator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  direct(:blog) { 'https://blog.fractalsoft.org' }
   direct :manifest do
     '/manifest.json'
   end
@@ -16,12 +17,13 @@ Rails.application.routes.draw do
   end
 
   scope ':locale', locale: /#{I18n.available_locales.join('|')}/ do
-    # resources :projects, only: [:index, :show]
-    resources :people, path: 'team', only: [:index, :show]
-    resources :contact_forms, path: 'contact-forms', only: [:new, :create]
     get :cieszyn, controller: 'cieszyn/welcome', action: :show
-    root 'home#index', as: :localized_root
   end
 
-  root to: 'home#index'
+  localized do
+    # resources :projects, only: [:index, :show]
+    resources :contact_forms, only: [:new, :create], path_names: { new: 'new_message' }
+    resources :people, only: [:index, :show]
+    root to: 'home#index'
+  end
 end
