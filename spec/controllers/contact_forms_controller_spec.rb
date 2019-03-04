9e08b01c80a3b69c87974ce104638e0f17bc186a
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2019-03-04 16:16:37 +0100

    Chore/localized routes (#79)
    * Translate routes with route_translator
    
    * Update route_translator gem
    
    * Fix failed tests
    
    * Clean up
    
    * Clean up with locales

--- spec/controllers/contact_forms_controller_spec.rb
+++ spec/controllers/contact_forms_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ContactFormsController, type: :controller do
   describe 'POST #create' do
+    let(:locale) { 'en' }
     let(:params) do
       { name: rand_text, email: 'test@example.com', message: rand_text }
     end
@@ -11,18 +12,18 @@ RSpec.describe ContactFormsController, type: :controller do
     end
 
     it 'has ContactForm object' do
-      post :create, params: { contact_form: params, locale: :en }
+      post :create, params: { contact_form: params, locale: locale }
       expect(assigns(:contact_form)).to be_a(ContactForm)
     end
 
     it 'send mail to application owner' do
       expect_any_instance_of(ContactForm).to receive(:deliver)
-      post :create, params: { contact_form: params, locale: :en }
+      post :create, params: { contact_form: params, locale: locale }
     end
 
     it 'redirect back to main page' do
-      post :create, params: { contact_form: params, locale: :en }
-      expect(response).to redirect_to root_path
+      post :create, params: { contact_form: params, locale: locale }
+      expect(response).to redirect_to(root_en_path)
     end
   end
 end
