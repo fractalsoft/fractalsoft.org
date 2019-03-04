7ebc1ab08c8fded7ba4c6fcb8f1c8ead40a05de7
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2019-03-04 16:16:37 +0100

    Chore/localized routes (#79)
    * Translate routes with route_translator
    
    * Update route_translator gem
    
    * Fix failed tests
    
    * Clean up
    
    * Clean up with locales

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe 'GET index' do
-    it 'render home template' do
-      get :index, params: { locale: :en }
-      expect(response).to render_template(:index)
-    end
+    I18n.available_locales.each do |locale|
+      context "locale #{locale}" do
+        let(:params) { { locale: locale.to_s } }
 
-    it 'has HomeFacade object' do
-      get :index, params: { locale: :en }
-      expect(assigns(:home)).to be_an(HomeFacade)
+        it 'render home template' do
+          get :index, params: params
+          expect(response).to render_template(:index)
+        end
+
+        it 'has HomeFacade object' do
+          get :index, params: params
+          expect(assigns(:home)).to be_an(HomeFacade)
+        end
+      end
     end
   end
 end
