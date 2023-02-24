3deb3fa5fe7634f28df8d3d5d7b52d89593ceffe
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-24 15:55:23 +0100

    Add sinatra to technology
    We recently worked on a project with sinatra
    and we want to be able to add this technology to our profiles.
    
    [1]: https://sinatrarb.com/

---
+++ db/seeds/technology/sinatra.rb
@@ -0,0 +1,9 @@
+# Sinatra
+# https://sinatrarb.com/
+technology_sinatra = Technology.where(slug: 'sinatra').first_or_initialize
+technology_sinatra.assign_attributes(
+  name: 'sinatra',
+  title: 'Sinatra',
+)
+technology_sinatra.save
+technology_sinatra.backend!
