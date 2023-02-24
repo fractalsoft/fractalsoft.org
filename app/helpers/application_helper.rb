1cb81c8052f3b5d3514077099af2dd169b8cc381
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-24 10:39:31 +0100

    Add helper to generate embedded SVG
    I'm adding a method
    that deconstructs the content of an SVG body with Nokogiri.
    This allows us to specify additional options and a title.
    
    Since we trust the data we input,
    I disabled cop Rails/OutputSafety in this method. [1]
    
    [1]: https://docs.rubocop.org/rubocop-rails/cops_rails.html#railsoutputsafety

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def default_meta_tags
     I18n.t(:meta).merge(separator: '|', author: '/humans.txt', reverse: true)
   end
+
+  # rubocop:disable Rails/OutputSafety
+  def embedded_svg(svg_body, options: {}, title: nil)
+    doc = Nokogiri::HTML::DocumentFragment.parse(svg_body)
+    svg = doc.at_css 'svg'
+
+    svg.add_child("<title>#{title}</title>") if title
+    options.each { |attribute, value| svg[attribute.to_s] = value }
+
+    doc.to_html.html_safe
+  end
+  # rubocop:enable Rails/OutputSafety
 end
