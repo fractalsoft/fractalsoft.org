a22116672a91cf45da03b7dddb95c81ba029780f
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-27 16:23:24 +0100

    Update SQLite icons
    Existing SQLite icons share ids with other embedded SVGs.
    This is a problem because the colors of the icon change.
    New SVG files do not cause this problem.

--- db/seeds/technology/sqlite.rb
+++ db/seeds/technology/sqlite.rb
@@ -1,11 +1,13 @@
 # SQLite
 # https://sqlite.org/
 technology_sqlite = Technology.where(slug: 'sqlite').first_or_initialize
-icon_sqlite_original_svg = File.read('./db/seeds/technology/svg/sqlite.svg')
+icon_sqlite_original_svg = File.read('./db/seeds/technology/svg/sqlite-original.svg')
+icon_sqlite_wordmark_svg = File.read('./db/seeds/technology/svg/sqlite-original-wordmark.svg')
 technology_sqlite.assign_attributes(
   name: 'sqlite',
   title: 'SQLite',
-  icon: icon_sqlite_original_svg
+  icon: icon_sqlite_original_svg,
+  icon_wordmark: icon_sqlite_wordmark_svg
 )
 technology_sqlite.save
 technology_sqlite.database!
