868941c606fa62fc5ed3a3699636faa7e38ff28d
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-27 16:05:03 +0100

    Update Unity icons
    Using embedded SVG for an existing icon crops it because it's too big.
    That's why I change the icons to different ones.

--- db/seeds/technology/unity.rb
+++ db/seeds/technology/unity.rb
@@ -1,11 +1,13 @@
-# Unity
+# Unity (game engine)
 # https://unity.com/
 technology_unity = Technology.where(slug: 'unity').first_or_initialize
-icon_unity_original_svg = File.read('./db/seeds/technology/svg/unity3d-original.svg')
+icon_unity_original_svg = File.read('./db/seeds/technology/svg/unity-original.svg')
+icon_unity_wordmark_svg = File.read('./db/seeds/technology/svg/unity-original-wordmark.svg')
 technology_unity.assign_attributes(
   name: 'unity',
   title: 'Unity',
-  icon: icon_unity_original_svg
+  icon: icon_unity_original_svg,
+  icon_wordmark: icon_unity_wordmark_svg
 )
 technology_unity.save
 technology_unity.gamedev!
