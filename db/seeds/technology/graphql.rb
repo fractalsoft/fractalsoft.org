31f9d0b7e3be976e53e228ddea204e8479c326c4
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 08:34:49 +0100

    Update GraphQL icons
    The existing GraphQL icon was oversized.
    To be consistent I change the icons to match the rest.

--- db/seeds/technology/graphql.rb
+++ db/seeds/technology/graphql.rb
@@ -1,11 +1,13 @@
 # GraphQL
 # https://graphql.org/
 technology_graphql = Technology.where(slug: 'graphql').first_or_initialize
-icon_graphql_svg = File.read('./db/seeds/technology/svg/graphql.svg')
+icon_graphql_plain_svg = File.read('./db/seeds/technology/svg/graphql-plain.svg')
+icon_graphql_wordmark_svg = File.read('./db/seeds/technology/svg/graphql-plain-wordmark.svg')
 technology_graphql.assign_attributes(
   name: 'graphql',
   title: 'GraphQL',
-  icon: icon_graphql_svg
+  icon: icon_graphql_plain_svg,
+  icon_wordmark: icon_graphql_wordmark_svg
 )
 technology_graphql.save
 technology_graphql.tool!
