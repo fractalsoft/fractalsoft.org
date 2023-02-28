be5d5757304515c550cd5858bb9cf476bf6e78ec
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 09:24:19 +0100

    Update missing RSpec icon
    We often use RSpec in projects,
    so we want to be up to date.

--- db/seeds/technology/rspec.rb
+++ db/seeds/technology/rspec.rb
@@ -1,11 +1,13 @@
 # Rspec
 # https://rspec.info/
 technology_rspec = Technology.where(slug: 'rspec').first_or_initialize
-icon_rspec_original_svg = File.read('./db/seeds/technology/svg/rspec.svg')
+icon_rspec_original_svg = File.read('./db/seeds/technology/svg/rspec-original.svg')
+icon_rspec_wordmark_svg = File.read('./db/seeds/technology/svg/rspec-original-wordmark.svg')
 technology_rspec.assign_attributes(
   name: 'rspec',
   title: 'RSpec',
-  icon: icon_rspec_original_svg
+  icon: icon_rspec_original_svg,
+  icon_wordmark: icon_rspec_wordmark_svg
 )
 technology_rspec.save
 technology_rspec.testing!
