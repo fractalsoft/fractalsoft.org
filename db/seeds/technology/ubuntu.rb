aa70cc9537beef12f3c2dea8f696cd1a5eaee06d
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 11:49:55 +0100

    fixup! Create technology record for Ubuntu

--- db/seeds/technology/ubuntu.rb
+++ db/seeds/technology/ubuntu.rb
@@ -1,12 +1,12 @@
 # Ubuntu
 # https://ubuntu.com/
 technology_ubuntu = Technology.where(slug: 'ubuntu').first_or_initialize
-icon_ubuntu_original_svg = File.read('./db/seeds/technology/svg/ubuntu-original.svg')
-icon_ubuntu_wordmark_svg = File.read('./db/seeds/technology/svg/ubuntu-original-wordmark.svg')
+icon_ubuntu_plain_svg = File.read('./db/seeds/technology/svg/ubuntu-plain.svg')
+icon_ubuntu_wordmark_svg = File.read('./db/seeds/technology/svg/ubuntu-plain-wordmark.svg')
 technology_ubuntu.assign_attributes(
   name: 'ubuntu',
   title: 'Ubuntu',
-  icon: icon_ubuntu_original_svg,
+  icon: icon_ubuntu_plain_svg,
   icon_wordmark: icon_ubuntu_wordmark_svg
 )
 technology_ubuntu.save
