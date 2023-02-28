6e8b4e43c83fa3cab403f839a43d39169c89b2fb
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 14:03:07 +0100

    Create technology record for Bootstrap
    We supplement the list of technologies with missing records.

---
+++ db/seeds/technology/bootstrap.rb
@@ -0,0 +1,13 @@
+# Bootstrap
+# https://getbootstrap.com/
+technology_bootstrap = Technology.where(slug: 'boostrap').first_or_initialize
+icon_bootstrap_original_svg = File.read('./db/seeds/technology/svg/bootstrap-original.svg')
+icon_bootstrap_wordmark_svg = File.read('./db/seeds/technology/svg/bootstrap-original-wordmark.svg')
+technology_bootstrap.assign_attributes(
+  name: 'Bootstrap',
+  title: 'Bootstrap',
+  icon: icon_bootstrap_original_svg,
+  icon_wordmark: icon_bootstrap_wordmark_svg
+)
+technology_bootstrap.save
+technology_bootstrap.web!
