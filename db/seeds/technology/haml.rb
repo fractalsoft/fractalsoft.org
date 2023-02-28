8335d9e8648635ae3a113694086292e4d269ae6b
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 15:45:01 +0100

    Update Haml icons
    We had a wordmark icon.
    But we didn't have the usual Haml logo.
    However, I noticed
    that such an icon is in the vscode icon repository. [1]
    
    [1]: https://github.com/vscode-icons/vscode-icons

--- db/seeds/technology/haml.rb
+++ db/seeds/technology/haml.rb
@@ -1,10 +1,12 @@
 # Haml
 # http://haml.info/
 technology_haml = Technology.where(slug: 'haml').first_or_initialize
-icon_haml_wordmark_svg = File.read('./db/seeds/technology/svg/haml-logo.svg')
+icon_haml_svg = File.read('./db/seeds/technology/svg/haml.svg')
+icon_haml_wordmark_svg = File.read('./db/seeds/technology/svg/haml-wordmark.svg')
 technology_haml.assign_attributes(
   name: 'haml',
   title: 'Haml',
+  icon: icon_haml_svg,
   icon_wordmark: icon_haml_wordmark_svg
 )
 technology_haml.save
