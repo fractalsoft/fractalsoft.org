8d95244e5eb9c106e05aecae4ecc32fe898c2236
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 15:27:50 +0100

    Add missing Slim icon
    Slim does not have an official SVG logo.
    That's why we missed the icon.
    
    However, I noticed that there is an icon repo for vscode [1]
    and Slim is there.
    We apply "Worse is Better". [2]
    
    [1]: https://github.com/vscode-icons/vscode-icons
    [2]: https://en.wikipedia.org/wiki/Worse_is_better

--- db/seeds/technology/slim.rb
+++ db/seeds/technology/slim.rb
@@ -1,9 +1,11 @@
 # Slim
-# http://slim-lang.com/
+# https://github.com/slim-template/slim
 technology_slim = Technology.where(slug: 'slim').first_or_initialize
+icon_slim_svg = File.read('./db/seeds/technology/svg/slim.svg')
 technology_slim.assign_attributes(
-  name: 'slim',
-  title: 'Slim'
+  name: 'Slim',
+  title: 'Slim Template Language',
+  icon: icon_slim_svg
 )
 technology_slim.save
 technology_slim.web!
