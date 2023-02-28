895a0af65164dbdf06ad7f187ed5579a87a32743
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-02-28 09:38:37 +0100

    Update Gatsby icons
    Update the icon to a newer and compressed version.

--- db/seeds/technology/gatsby.rb
+++ db/seeds/technology/gatsby.rb
@@ -1,11 +1,13 @@
 # Gatsby
 # https://www.gatsbyjs.com/
 technology_gatsby = Technology.where(slug: 'gatsby').first_or_initialize
-icon_gatsby_svg = File.read('./db/seeds/technology/svg/gatsby.svg')
+icon_gatsby_original_svg = File.read('./db/seeds/technology/svg/gatsby-original.svg')
+icon_gatsby_wordmark_svg = File.read('./db/seeds/technology/svg/gatsby-original-wordmark.svg')
 technology_gatsby.assign_attributes(
   name: 'gatsby',
   title: 'Gatsby',
-  icon: icon_gatsby_svg
+  icon: icon_gatsby_original_svg,
+  icon_wordmark: icon_gatsby_wordmark_svg
 )
 technology_gatsby.save
 technology_gatsby.frontend!
