7e7adfb94f506e007238ad8cd9fedd620ecec7b6
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-01 15:37:18 +0100

    Fix SVG icon for Apache Cordova
    A properly embedded SVG file should have a unique set of identifiers.
    I added prefixes to the IDs
    because SVG for Apache Cordova did not have them.

--- db/seeds/technology/cordova.rb
+++ db/seeds/technology/cordova.rb
@@ -1,10 +1,10 @@
-# Cordova
+# Apache Cordova
 # https://cordova.apache.org/
 technology_cordova = Technology.where(slug: 'cordova').first_or_initialize
-icon_cordova_original_svg = File.read('./db/seeds/technology/svg/cordova.svg')
+icon_cordova_original_svg = File.read('./db/seeds/technology/svg/apache_cordova.svg')
 # icon_cordova_wordmark_svg = File.read('./db/seeds/technology/svg/cordova-wordmark.svg')
 technology_cordova.assign_attributes(
-  name: 'cordova',
+  name: 'Cordova',
   title: 'Apache Cordova',
   icon: icon_cordova_original_svg
   # icon_wordmark: icon_cordova_wordmark_svg
