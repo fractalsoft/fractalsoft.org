dedd0395e162e48843bea5a58ee483b7bcd0e85e
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-02 16:42:22 +0100

    Add TMC Ops (Bode & Drift) project to the list
    It's time to add this project to our list of projects after three years.
    
    🎉

--- db/seeds/05_projects.rb
+++ db/seeds/05_projects.rb
@@ -389,3 +389,35 @@ Mikroserwisy zbudowane na Sinatrze i obsługiwane przez Apache Kafka.
 andy.subtitle = 'platforma do zakupu i dostawy napojów'
 
 andy.save
+
+# TMC Hospitality: Bode & Drift
+tmc_ops = Project.where(slug: 'tmc-ops').first_or_initialize
+tmc_ops.assign_attributes(
+  display: true,
+  dofollow: false,
+  title: 'Bode & Drift Ops',
+  url: nil,
+  year: 2020
+)
+
+I18n.locale = :en
+tmc_ops.description = %()
+tmc_ops.introduction = %(
+The Ops application allowed the Bode and Drift chain of hotels in North and Central America to be fully automated.
+Door lock control, automatic email and SMS notifications from booking to check-in to final guest check-out.
+Integrations with other hotel management platforms (StayNTouch, SynXis) and other services (Google, Facebook).
+All of this adds up to thousands of events combined into neatly organized streams in the Rails Event Store.
+)
+tmc_ops.subtitle = 'hotel management application'
+
+I18n.locale = :pl
+tmc_ops.description = %()
+tmc_ops.introduction = %(
+Aplikacja Ops pozwoliła na pełną automatyzację sieci hoteli Bode oraz Drift w Ameryce Północnej i Środkowej.
+Sterowanie zamkami do drzwi, automatyczne powiadomienia e-mail i SMS od rezerwacji, aż do zameldowania i ostatecznego wymeldowania.
+Integracje z innymi platformami do zarządzania hotelami (StayNTouch, SynXis) i innymi usługami (Google, Facebook).
+Wszystko to składa się na tysiące wydarzeń połączonych w zgrabne strumienie w Rails Event Store.
+)
+tmc_ops.subtitle = 'aplikacja do zarządzania hotelami'
+
+tmc_ops.save
