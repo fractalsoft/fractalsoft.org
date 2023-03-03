2fc9bf60e26fcb53ce2e684ac48c856e1665c3b6
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-03 14:24:41 +0100

    Fix SVG filename for Cordova record
    We forgot to change the SVG call after changing the SVG file.

--- db/seeds/technology/cordova.rb
+++ db/seeds/technology/cordova.rb
@@ -2,7 +2,7 @@
 # https://cordova.apache.org/
 technology_cordova = Technology.where(slug: 'cordova').first_or_initialize
 icon_cordova_original_svg = File.read('./db/seeds/technology/svg/apache_cordova.svg')
-# icon_cordova_wordmark_svg = File.read('./db/seeds/technology/svg/cordova-wordmark.svg')
+# icon_cordova_wordmark_svg = File.read('./db/seeds/technology/svg/apache_cordova-wordmark.svg')
 technology_cordova.assign_attributes(
   name: 'Cordova',
   title: 'Apache Cordova',
