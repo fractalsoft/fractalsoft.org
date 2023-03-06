3de130a0a2af2f5dc1846ebb8e8db2eef934bf01
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-06 14:01:57 +0100

    Change the number into words
    Our variable names did not please Rubocop.
    They contained the year written in numerical form. [1]
    I have decided that in this particular case
    it would be better to have the year in words,
    but in an abbreviated form.
    
    [1]: https://docs.rubocop.org/rubocop/cops_naming.html#namingvariablenumber

--- db/seeds/community/coderdojo.rb
+++ db/seeds/community/coderdojo.rb
@@ -1,12 +1,12 @@
 # CoderDojo
 coderdojo = Community.where(slug: 'coderdojo').first_or_create
 
-coderdojo_meeting_in_2015_youtube_url = 'https://www.youtube.com/embed/G_tTBd_ei3s'
-coderdojo_santa_claus_in_2015_youtube_url = 'https://www.youtube.com/embed/LK95K3IFP_s'
+coderdojo_meeting_in_twenty_fifteen_youtube_url = 'https://www.youtube.com/embed/G_tTBd_ei3s'
+coderdojo_santa_claus_in_twenty_fifteen_youtube_url = 'https://www.youtube.com/embed/LK95K3IFP_s'
 coderdojo_preparing_coolest_project_vimeo_url = 'https://player.vimeo.com/video/221539037'
 
-coderdojo_meeting_in_2015 = youtube_iframe(coderdojo_meeting_in_2015_youtube_url)
-coderdojo_santa_claus_in_2015 = youtube_iframe(coderdojo_santa_claus_in_2015_youtube_url)
+coderdojo_meeting_in_twenty_fifteen = youtube_iframe(coderdojo_meeting_in_twenty_fifteen_youtube_url)
+coderdojo_santa_claus_in_twenty_fifteen = youtube_iframe(coderdojo_santa_claus_in_twenty_fifteen_youtube_url)
 coderdojo_preparing_coolest_project = vimeo_iframe(coderdojo_preparing_coolest_project_vimeo_url)
 
 coderdojo_logo = File.new('db/seeds/community/coderdojo/coderdojo-logo.png')
@@ -29,9 +29,9 @@ We focus on acquiring knowledge and sharing it with others.
 
 You can read about what we currently do at CoderDojo in Gliwice on our blog.
 
-#{coderdojo_meeting_in_2015}
+#{coderdojo_meeting_in_twenty_fifteen}
 
-#{coderdojo_santa_claus_in_2015}
+#{coderdojo_santa_claus_in_twenty_fifteen}
 
 #{coderdojo_preparing_coolest_project}
 )
@@ -60,11 +60,12 @@ Zależy nam, aby młode pokolenie potrafiło kreatywnie wykorzystywać nowe tech
 Chcemy aby dzieci wiedziały jak działa Internet, z czego składa się komputer oraz jak zbudować własnego robota.
 Stawiamy na zdobywanie wiedzy oraz dzielenie się nią z innymi.
 
-O tym czym aktualnie zajmujemy się na [CoderDojo w Gliwicach](https://blog.fractalsoft.org/pl/coderdojo) możecie poczytać na naszym blogu.
+O tym czym aktualnie zajmujemy się na [CoderDojo w Gliwicach](https://blog.fractalsoft.org/pl/coderdojo)
+możecie poczytać na naszym blogu.
 
-#{coderdojo_meeting_in_2015}
+#{coderdojo_meeting_in_twenty_fifteen}
 
-#{coderdojo_santa_claus_in_2015}
+#{coderdojo_santa_claus_in_twenty_fifteen}
 
 #{coderdojo_preparing_coolest_project}
 )
