cd4b12a71a675b1df3cfb5a2223e8d06026fb359
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-06 13:15:52 +0100

    Price list seeds refactoring
    Rubocop had many warnings to the file responsible
    for preparing the data from the price list.
    Refactoring this code was the easiest solution.

--- db/seeds/03_price_list.rb
+++ db/seeds/03_price_list.rb
@@ -1,18 +1,22 @@
 I18n.locale = :pl
 ComputerFixService.delete_all
-ComputerFixService.create position:  1, currency: 'PLN', name: 'Diagnoza z wstępną wyceną', price: 50.0
-ComputerFixService.create position:  2, currency: 'PLN', name: 'Instalacja systemu operacyjnego, sterowników, programów użytkowych', price: 80.0
-ComputerFixService.create position:  3, currency: 'PLN', name: 'Czyszczenie wentylatora, wymiana pasty', price: 50.0
-ComputerFixService.create position:  4, currency: 'PLN', name: 'Naprawa płyty głównej (nie BGA)', price: 150.0
-ComputerFixService.create position:  5, currency: 'PLN', name: 'Czyszczenie laptopa po zalaniu', price: 120.0
-ComputerFixService.create position:  6, currency: 'PLN', name: 'Reballing BGA', price: 260.0
-ComputerFixService.create position:  7, currency: 'PLN', name: 'Wymiana układu BGA (bez ceny części)', price: 240.0
-ComputerFixService.create position:  8, currency: 'PLN', name: "Aktualizacja BIOS'a", price: 30.0
-ComputerFixService.create position:  9, currency: 'PLN', name: "Aktualizacja BIOS'a (programowanie wlutowanego układu)", price: 60.0
-ComputerFixService.create position: 10, currency: 'PLN', name: 'Naprawa zasilacza', price: 60.0
-ComputerFixService.create position: 11, currency: 'PLN', name: 'Naprawa gniazda zasilania, USB, audio', price: 100.0
-ComputerFixService.create position: 12, currency: 'PLN', name: 'Wymiana lub naprawa innych części w laptopie', price: 150.0
-ComputerFixService.create position: 13, currency: 'PLN', name: 'Usuwanie wirusów oraz złośliwego oprogramowania', price: 60.0
-ComputerFixService.create position: 14, currency: 'PLN', name: 'Naprawa monitora LCD', price: 250.0
-ComputerFixService.create position: 15, currency: 'PLN', name: 'Archiwizacja danych', price: 30.0
+[
+  { name: 'Diagnoza z wstępną wyceną', price: 50.0 },
+  { name: 'Instalacja systemu operacyjnego, sterowników, programów użytkowych', price: 80.0 },
+  { name: 'Czyszczenie wentylatora, wymiana pasty', price: 50.0 },
+  { name: 'Naprawa płyty głównej (nie BGA)', price: 150.0 },
+  { name: 'Czyszczenie laptopa po zalaniu', price: 120.0 },
+  { name: 'Reballing BGA', price: 260.0 },
+  { name: 'Wymiana układu BGA (bez ceny części)', price: 240.0 },
+  { name: "Aktualizacja BIOS'a", price: 30.0 },
+  { name: "Aktualizacja BIOS'a (programowanie wlutowanego układu)", price: 60.0 },
+  { name: 'Naprawa zasilacza', price: 60.0 },
+  { name: 'Naprawa gniazda zasilania, USB, audio', price: 100.0 },
+  { name: 'Wymiana lub naprawa innych części w laptopie', price: 150.0 },
+  { name: 'Usuwanie wirusów oraz złośliwego oprogramowania', price: 60.0 },
+  { name: 'Naprawa monitora LCD', price: 250.0 },
+  { name: 'Archiwizacja danych', price: 30.0 }
+].each_with_index do |params, index|
+  ComputerFixService.create position: index + 1, currency: 'PLN', **params
+end
 I18n.locale = :en
