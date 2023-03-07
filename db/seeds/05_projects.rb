7cef206553e8686dc6878c1ce5096b94270fb01b
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-07 12:15:59 +0100

    Add Julinek AR game to the project list
    Greg has built a game in Unity that uses AR (Augmented Reality).
    And he want to brag about it.
    
    🎉

--- db/seeds/05_projects.rb
+++ db/seeds/05_projects.rb
@@ -425,3 +425,39 @@ Wszystko to składa się na tysiące wydarzeń połączonych w zgrabne strumieni
 tmc_ops.subtitle = 'aplikacja do zarządzania hotelami'
 
 tmc_ops.save
+
+# Julinek AR
+# https://apps.apple.com/us/developer/grzegorz-lisowski/id1665804173
+# https://play.google.com/store/apps/details?id=com.Trusense.JulinekWyspy
+julinek_ar = Project.where(slug: 'julinek-ar').first_or_initialize
+julinek_ar.assign_attributes(
+  display: true,
+  dofollow: false,
+  title: 'Julinek AR',
+  url: 'https://apps.apple.com/us/developer/grzegorz-lisowski/id1665804173',
+  year: 2022
+)
+
+I18n.locale = :en
+julinek_ar.description = %()
+julinek_ar.introduction = %(
+A simple game based on augmented reality.
+The goal is to explore the former circus base and learn while having fun.
+Technologically, the game is based on Unity engine and written in C#.
+It is a free download from the Apple Store,
+but requires AR markers, which are available in Julinek Park.
+)
+julinek_ar.subtitle = 'augmented reality circus game'
+
+I18n.locale = :pl
+julinek_ar.description = %()
+julinek_ar.introduction = %(
+Prosta gra oparta na rzeczywistości rozszerzonej.
+Celem jest zwiedzanie dawnej bazy cyrkowej i nauka poprzez zabawę.
+Technologicznie gra bazuje na silniku Unity i napisana jest w C#.
+Można ją pobrać za darmo ze sklepu Apple,
+jednak do prawidłowego działania potrzebne są markery AR dostępne w Julinek Park.
+)
+julinek_ar.subtitle = 'cyrkowa gra w rzeczywistości rozszerzonej'
+
+julinek_ar.save
