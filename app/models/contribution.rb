0736f06981fcc10c650aee149e3118d87f610cda
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-03-09 13:13:46 +0100

    Enable fragment caching
    We have a form on the main page,
    so we should not enable page caching there.
    
    But the solution in this case is fragment caching [1]
    and russian doll caching. [2]
    
    Here are the logs that show that our caching works:
    
    ```
      Rendered collection of people/_person.slim [4 times] (Duration: 31.6ms | Allocations: 14751)
    Write fragment views/people/_team:d7fe0457bd4886efab7fa2c2871aa9a4/people/6922d621-8a33-4bd7-b39e-b6b55cf748c0/person/translations/1-20230308123423803328/people/20b34aef-d4e5-4c56-a920-8db913494db8/person/translations/5-20230308123424330422/people/d50660db-e05b-4f9c-8eb2-c28ba0759b42/person/translations/7-20230308123424633667/people/bb27a995-9214-427c-8187-3d014a9350ed/person/translations/3-20230308123424084848 (1.3ms)
      Rendered people/_team.slim (Duration: 135.9ms | Allocations: 46102) [cache miss]
    
    ...
    
    Read fragment views/people/_team:d7fe0457bd4886efab7fa2c2871aa9a4/people/6922d621-8a33-4bd7-b39e-b6b55cf748c0/person/translations/1-20230308123423803328/people/20b34aef-d4e5-4c56-a920-8db913494db8/person/translations/5-20230308123424330422/people/d50660db-e05b-4f9c-8eb2-c28ba0759b42/person/translations/7-20230308123424633667/people/bb27a995-9214-427c-8187-3d014a9350ed/person/translations/3-20230308123424084848 (4.6ms)
      Rendered people/_team.slim (Duration: 10.9ms | Allocations: 2383) [cache hit]
    ```
    
    I also ensured that changing related models updates the parent models.
    
    [1]: https://guides.rubyonrails.org/caching_with_rails.html#fragment-caching
    [2]: https://guides.rubyonrails.org/caching_with_rails.html#russian-doll-caching

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,5 +2,5 @@ class Contribution < ApplicationRecord
   translates :name
 
   belongs_to :project, optional: true
-  belongs_to :person, optional: true
+  belongs_to :person, optional: true, touch: true
 end
--- app/models/technological_skill.rb
+++ app/models/technological_skill.rb
@@ -1,4 +1,4 @@
 class TechnologicalSkill < ApplicationRecord
-  belongs_to :person
+  belongs_to :person, touch: true
   belongs_to :technology
 end
