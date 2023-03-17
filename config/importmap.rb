34d1487f042f1c2b3f8d410d4689e50ab59080ab
Grzegorz Lisowski <grzegorz.lisowski@futurelearn.com>
2023-03-17 16:38:22 +0100

    Move package.json to importmaps
    Since we are moving into impormaps we want to move all our
    old dependencies which we were fetching using npm and yarn
    to instead pining them using importmaps and jspm.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,14 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.3/dist/leaflet-src.js"
+pin "postcss", to: "https://ga.jspm.io/npm:postcss@8.4.21/lib/postcss.mjs"
+pin "buffer", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/buffer.js"
+pin "fs", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/fs.js"
+pin "nanoid/non-secure", to: "https://ga.jspm.io/npm:nanoid@3.3.4/non-secure/index.js"
+pin "path", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/path.js"
+pin "picocolors", to: "https://ga.jspm.io/npm:picocolors@1.0.0/picocolors.browser.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
+pin "source-map-js", to: "https://ga.jspm.io/npm:source-map-js@1.0.2/source-map.js"
+pin "url", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/url.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.4/dist/jquery.js"
