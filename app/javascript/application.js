ecc8a9e14cc9ae9c05bd98035c7cb30636cccdac
Grzegorz Lisowski <grzegorz.lisowski@futurelearn.com>
2023-03-17 16:46:26 +0100

    Remove old pack file
    Since we removed webpacker we need to move any js start logic
    form the packs file to the new application.js file from importmaps.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
