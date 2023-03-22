384b8bfed936a8b8fadd8931427ea83174f6e0c3
Grzegorz Lisowski <grzegorz.lisowski@futurelearn.com>
2023-03-22 09:29:39 +0100

    Convert javascript from coffeescript
    importmaps don't have any way of converting files before they
    are served so we are rewriting all our coffee script files
    into javascript and moving them into the new file structure.

---
+++ app/javascript/fractalsoft/contact_forms.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+  $('.btn-map').click(function() {
+    const target = $(this).data('target');
+    $('.btn-map').removeClass('active');
+    $(this).addClass('active');
+    $('iframe.map').addClass('map-display-none');
+    return $(target).removeClass('map-display-none');
+  });
+
+  return $('#contact_form').ready(() => $('#contact_form_browser_info').val('' + browserInfo.json()));
+});
