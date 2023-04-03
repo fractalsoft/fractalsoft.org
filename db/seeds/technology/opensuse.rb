000b112cecd134409bfa9a774104d1cd27554dd2
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-04-03 13:53:27 +0200

    Create technology record for openSUSE
    We supplement the list of technologies with missing record.

---
+++ db/seeds/technology/opensuse.rb
@@ -0,0 +1,13 @@
+# openSUSE
+# https://www.opensuse.org/
+technology_opensuse = Technology.where(slug: 'opensuse').first_or_initialize
+icon_opensuse_original_svg = File.read('./db/seeds/technology/svg/opensuse-original.svg')
+icon_opensuse_wordmark_svg = File.read('./db/seeds/technology/svg/opensuse-original-wordmark.svg')
+technology_opensuse.assign_attributes(
+  name: 'openSUSE',
+  title: 'openSUSE',
+  icon: icon_opensuse_original_svg,
+  icon_wordmark: icon_opensuse_wordmark_svg
+)
+technology_opensuse.save
+technology_opensuse.os!
