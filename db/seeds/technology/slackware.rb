90fa05ccd9807420c583b7ae3159b80d3df79cdd
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-04-03 12:55:47 +0200

    Create technology record for Slackware
    We supplement the list of technologies with missing record.

---
+++ db/seeds/technology/slackware.rb
@@ -0,0 +1,11 @@
+# Slackware
+# http://www.slackware.com/
+technology_slackware_linux = Technology.where(slug: 'slackware').first_or_initialize
+icon_slackware_linux_original_svg = File.read('./db/seeds/technology/svg/slackware.svg')
+technology_slackware_linux.assign_attributes(
+  name: 'slackware',
+  title: 'Slackware',
+  icon: icon_slackware_linux_original_svg
+)
+technology_slackware_linux.save
+technology_slackware_linux.os!
