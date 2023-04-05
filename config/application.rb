29c6d77fbe6a72f742cb162a9ab25351a8ae4643
Grzegorz Lisowski <grzegorz.lisowski@futurelearn.com>
2023-04-05 16:39:07 +0200

    Cleanup Rails update changes
    Rails update made a lot of changes that don't bring anything
    to our codebase like changing single quotes to double quotes.
    
    To make looking for the changes easier I cleaned up the changes
    made by the rails update to add only the minimal changes.

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,17 @@
-require_relative "boot"
-
-require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+require_relative 'boot'
+require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module FractalSoft
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.action_controller.page_cache_directory = Rails.public_path.join('cached_pages')
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :pl]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
+    config.middleware.use Rack::Deflater
   end
 end
