b100b38dc1dfa3ec0dd2dd4699c8fbfe22b53d3f
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-04-05 09:46:14 +0200

    Add icon to Sinatra technology
    We didn't have SVG for Sinatra framework. [1]
    I downloaded the file from the SVGrepo.com
    and optimized it with the svgo tool.
    
    [1]: https://sinatrarb.com/

--- db/seeds/technology/sinatra.rb
+++ db/seeds/technology/sinatra.rb
@@ -1,9 +1,11 @@
 # Sinatra
 # https://sinatrarb.com/
 technology_sinatra = Technology.where(slug: 'sinatra').first_or_initialize
+icon_sinatra_svg = File.read('./db/seeds/technology/svg/sinatra.svg')
 technology_sinatra.assign_attributes(
   name: 'sinatra',
-  title: 'Sinatra'
+  title: 'Sinatra',
+  icon: icon_sinatra_svg
 )
 technology_sinatra.save
 technology_sinatra.backend!
