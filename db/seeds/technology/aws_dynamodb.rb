7b891fec3de3aa58b1aa394ef8d10ff8307465a7
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-04-11 15:19:27 +0200

    Create technology record for Amazon DynamoDB
    > Fast, flexible NoSQL database service for single-digit millisecond
    > performance at any scale
    
    > Amazon DynamoDB is a fully managed, serverless, [1]
    > key-value NoSQL database designed to run high-performance
    > applications at any scale. DynamoDB offers built-in security,
    > continuous backups, automated multi-Region replication,
    > in-memory caching, and data import and export tools.
    
    [1]: https://aws.amazon.com/dynamodb/

---
+++ db/seeds/technology/aws_dynamodb.rb
@@ -0,0 +1,11 @@
+# Amazon DynamoDB
+# https://aws.amazon.com/dynamodb/
+technology_aws_dynamodb = Technology.where(slug: 'aws-dynamodb').first_or_initialize
+icon_aws_dynamodb_svg = File.read('./db/seeds/technology/svg/aws-dynamodb.svg')
+technology_aws_dynamodb.assign_attributes(
+  name: 'Amazon DynamoDB',
+  title: 'Amazon DynamoDB',
+  icon: icon_aws_dynamodb_svg
+)
+technology_aws_dynamodb.save
+technology_aws_dynamodb.amazon_web_services!
