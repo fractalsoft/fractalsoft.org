e6d9dcc0e6acf47ca83e42b8b1b6cc89a629c8ad
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-06-06 09:50:34 +0200

    Fill the experience rating for Woman On Rails
    Since we are changing the structure of our developer profile,
    we need numeric values for our technological skills.
    
    In cooperation with Woman on Rails,
    we have established a subjective ratings for her.
    
    Co-authored-by: Agnieszka Malaszkiewicz <womanonrails@users.noreply.github.com>
    Co-authored-by: Alex Malaszkiewicz <torrocus@users.noreply.github.com>

--- db/seeds/team/womanonrails.rb
+++ db/seeds/team/womanonrails.rb
@@ -43,56 +43,75 @@ womanonrails.save
 
 TechnologicalSkill.where(person_id: womanonrails.id).delete_all
 
-%w[
-  ruby-on-rails
-  ruby
-  rails-event-store
-  rspec
-  postgresql
-  postgis
-  leaflet
-  coffeescript
-  backbonejs
-  marionettejs
-  javascript
-  react
-  graphql
-  jquery
-  emberjs
-  git
-  mysql
-  sqlite
-  redis
-  docker
-  sass
-  less
-  html5
-  css3
-  slim
-  aws
-  cypress
-  ubuntu
-  heroku
-  github
-  selenium-webdriver
-  elasticsearch
-  solr
-  trello
-  taiga
-  pivotal-tracker
-  aws-api-gateway
-  aws-cloudwatch
-  aws-ec2
-  aws-lambda
-  aws-s3
-  aws-sns
-  aws-sqs
-  sidekiq
-].each_with_index do |slug, index|
+{
+  'ruby' => { experience_rating: 80 },
+  'ruby-on-rails' => { experience_rating: 70 },
+  'git' => { experience_rating: 80 },
+  'rails-event-store' => { experience_rating: 60 },
+  'sidekiq' => { experience_rating: 50 },
+
+  'postgresql' => { experience_rating: 70 },
+  'postgis' => { experience_rating: 60 },
+  'mysql' => { experience_rating: 60 },
+  'sqlite' => { experience_rating: 90 },
+  'elasticsearch' => { experience_rating: 50 },
+  'solr' => { experience_rating: 50 },
+  'redis' => { experience_rating: 50 },
+
+  'rspec' => { experience_rating: 70 },
+  'selenium-webdriver' => { experience_rating: 60 },
+  'cypress' => { experience_rating: 40 },
+
+  'github' => { experience_rating: 90 },
+  'heroku' => { experience_rating: 80 },
+  'pivotal-tracker' => { experience_rating: 80 },
+  'taiga' => { experience_rating: 50 },
+  'trello' => { experience_rating: 80 },
+
+  'ubuntu' => { experience_rating: 70 },
+  'docker' => { experience_rating: 60 },
+
+  'javascript' => { experience_rating: 60 },
+  'coffeescript' => { experience_rating: 60 },
+  'jquery' => { experience_rating: 70 },
+  'react' => { experience_rating: 60 },
+  'leaflet' => { experience_rating: 80 },
+  'backbonejs' => { experience_rating: 60 },
+  'emberjs' => { experience_rating: 60 },
+  'marionettejs' => { experience_rating: 60 },
+
+  'sass' => { experience_rating: 70 },
+  'css3' => { experience_rating: 60 },
+  'html5' => { experience_rating: 90 },
+  'slim' => { experience_rating: 90 },
+  'graphql' => { experience_rating: 60 },
+  'less' => { experience_rating: 70 },
+
+  'aws' => { experience_rating: 30 },
+  'aws-s3' => { experience_rating: 40 },
+  'aws-lambda' => { experience_rating: 40 },
+  'aws-api-gateway' => { experience_rating: 30 },
+  'aws-cloudwatch' => { experience_rating: 20 },
+  'aws-ec2' => { experience_rating: 30 },
+  'aws-sns' => { experience_rating: 30 },
+  'aws-sqs' => { experience_rating: 30 },
+
+  'rubocop' => { experience_rating: 90 },
+
+  'github-actions' => { experience_rating: 30 },
+  'circle-ci' => { experience_rating: 30 },
+
+  'jekyll' => { experience_rating: 60 },
+
+  'mathematica' => { experience_rating: 40 }
+}.each_with_index do |(slug, result), index|
   technology = Technology.find_by(slug:)
+  next unless technology
+
   TechnologicalSkill.where(
     person_id: womanonrails.id,
     position: index + 1,
+    rating: result[:experience_rating],
     technology_id: technology.id
   ).first_or_create
 end
