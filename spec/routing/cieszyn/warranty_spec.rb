7040ae45e0a187c3bcb261a1c38407c4087d4c2d
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-08-28 12:41:47 +0200

    Delete routes for computer service
    We are limiting activity of our PC service in Cieszyn.
    The computer service will no longer have an office for customers.

--- spec/routing/cieszyn/warranty_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Cieszyn::WarrantyController do
-  it 'routes to #show' do
-    path = 'cieszyn/warranty'
-    expect(get("/#{path}")).to route_to("#{path}#show", locale: :pl)
-  end
-end
