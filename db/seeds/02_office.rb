9abe68b982d3bea00406ca0c299820026f78b6c8
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-08-29 08:23:27 +0200

    Fix item numbers in office addresses
    After removing the address in Cieszyn,
    position number 1 on the list of office addresses disappeared.
    Therefore, on the contact page, the map is not displayed.
    Only selecting the appropriate address causes the map to be displayed.
    
    Updating the position in our data fixes this problem.

--- db/seeds/02_office.rb
+++ db/seeds/02_office.rb
@@ -6,7 +6,7 @@ office_address_gliwice_srednia.assign_attributes(
   latitude: BigDecimal('50.29539334461822'),
   longitude: BigDecimal('18.666247576475143'),
   map_link: 'https://www.openstreetmap.org/export/embed.html?bbox=18.66407632827759%2C50.294623988249924%2C18.668418824672703%2C50.296162688545294&layer=mapnik&marker=50.29539334461822%2C18.666247576475143',
-  position: 2,
+  position: 1,
   postcode: '44-100',
   street_with_number: 'ul. Średnia 24/5'
 )
@@ -29,7 +29,7 @@ office_address_gliwice_poniatowskiego.assign_attributes(
   latitude: BigDecimal('50.30249'),
   longitude: BigDecimal('18.69275'),
   map_link: 'https://www.openstreetmap.org/export/embed.html?bbox=18.69023591279984%2C50.30172935216233%2C18.694591820240024%2C50.30325069073261&layer=mapnik&marker=50.30249%2C18.69275',
-  position: 3,
+  position: 2,
   postcode: '44-100',
   street_with_number: 'ul. Księcia Józefa Poniatowskiego 29/4'
 )
