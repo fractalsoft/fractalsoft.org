07a2a3b38cb22b180e5c9e0cd76b6a98096f4f90
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-08-29 13:41:39 +0200

    Fix typo in slug of technology
    We missed the first "t" in the word Bootstrap.
    This change fixes this typo.

--- db/seeds/technology/bootstrap.rb
+++ db/seeds/technology/bootstrap.rb
@@ -1,6 +1,6 @@
 # Bootstrap
 # https://getbootstrap.com/
-technology_bootstrap = Technology.where(slug: 'boostrap').first_or_initialize
+technology_bootstrap = Technology.where(slug: 'bootstrap').first_or_initialize
 icon_bootstrap_original_svg = File.read('./db/seeds/technology/svg/bootstrap-original.svg')
 icon_bootstrap_wordmark_svg = File.read('./db/seeds/technology/svg/bootstrap-original-wordmark.svg')
 technology_bootstrap.assign_attributes(
