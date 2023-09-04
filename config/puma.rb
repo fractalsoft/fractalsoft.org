d21fb185ba1a3af77f5cb59d1bba2df810416c07
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-09-04 10:11:07 +0200

    Revert "Reduce the number of workers and threads in Puma"
    This reverts commit 007909a40bd6907836874b4c0939ffbcb0dd0994.

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
-workers ENV.fetch('WEB_CONCURRENCY') { 1 }
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 2 }
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
 preload_app!
