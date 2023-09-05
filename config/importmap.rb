38a5dea95cffbd1fa7f8951dfca70e0fc30f9f54
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-09-05 09:12:54 +0200

    Update Rails-related js packages
    We updated Rails a while ago,
    but we forgot about dependencies in importmaps.
    We want to be up to date, so we're doing it now.

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,7 +13,7 @@ pin "url", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/url.js"
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.7-2/app/assets/javascripts/activestorage.esm.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.7-2/lib/assets/compiled/rails-ujs.js"
 pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
-pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.4-3/app/assets/javascripts/activestorage.esm.js"
 pin_all_from "app/javascript/fractalsoft", under: "fractalsoft"
