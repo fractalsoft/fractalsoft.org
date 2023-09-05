f805f3d4ad80e0f9a41e8f7427e8ed0fdb5cf642
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-09-05 10:19:36 +0200

    Remove PostCSS dependencies
    Since we removed Post CSS,
    we don't need packages that depend on this library either.
    Therefore, after discussion in the team
    I unpinned the remaining dependencies.

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,12 +2,6 @@
 
 pin "application", preload: true
 pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet-src.js"
-pin "buffer", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/buffer.js"
-pin "fs", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/fs.js"
-pin "path", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/path.js"
-pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
-pin "source-map-js", to: "https://ga.jspm.io/npm:source-map-js@1.0.2/source-map.js"
-pin "url", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/url.js"
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
