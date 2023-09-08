bfb404b34464a9f98a1d1b89f934d4ddf470039b
Anna Bargiel <ania-hm@users.noreply.github.com>
2023-09-08 10:38:21 +0200

    Remove unused packages from importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import ujs from "@rails/ujs";
 import turbolinks from "turbolinks";
-import * as activeStorage from "@rails/activestorage";
 
-ujs.start();
 turbolinks.start();
-activeStorage.start();
 
 import "jquery";
 import "@popperjs/core";
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,5 @@ pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet-src.js"
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.7-2/app/assets/javascripts/activestorage.esm.js"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.7-2/lib/assets/compiled/rails-ujs.js"
 pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
 pin_all_from "app/javascript/fractalsoft", under: "fractalsoft"
