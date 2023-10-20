7d031503331f0014949b6cdc41eeffc75f3d682a
Anna Bargiel <ania-hm@users.noreply.github.com>
2023-10-20 10:15:59 +0200

    Update Rspec experience rating
    After adding missing tests and refactoring existing tests in purpura,
    my knowledge of working with Rspec has increased.

--- scripts/one_offs/update_profiles_data.rb
+++ scripts/one_offs/update_profiles_data.rb
@@ -11,15 +11,9 @@ rspec = Technology.find_by(slug: 'rspec')
 # rubocop:disable Rails/SkipsModelValidations
 TechnologicalSkill.upsert_all(
   [
-    { person_id: ania.id, rating: 12, technology_id: rspec.id }
+    { person_id: ania.id, rating: 17, technology_id: rspec.id }
   ],
   unique_by: [:person_id, :technology_id],
   update_only: [:rating]
 )
 # rubocop:enable Rails/SkipsModelValidations
-
-fractalsoft = Project.find_by(slug: 'fractalsoft.org')
-
-contribution = Contribution.create_with(name: 'Writing unit tests')
-                           .find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 8 })
-Globalize.with_locale(:pl) { contribution.update(name: 'Pisanie testów jednostkowych') }
