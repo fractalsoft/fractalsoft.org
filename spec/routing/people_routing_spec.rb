2b4cbf0e88781f42feb8011fc8f4c513dca053f2
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-10-24 11:55:57 +0200

    Add some routing tests

--- spec/routing/people_routing_spec.rb
+++ spec/routing/people_routing_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
-# RSpec.describe PeopleController, type: :routing do
-#   describe 'routing' do
-#   end
-# end
+RSpec.describe PeopleController do
+  describe 'routing' do
+    context 'with English language' do
+      it { is_expected.to route(:get, '/team').to(controller: :people, action: :index, locale: 'en') }
+      it { is_expected.to route(:get, '/team/1').to(controller: :people, action: :show, id: 1, locale: 'en') }
+    end
+
+    context 'with Polish language' do
+      it { is_expected.to route(:get, '/pl/zespol').to(controller: :people, action: :index, locale: 'pl') }
+      it { is_expected.to route(:get, '/pl/zespol/1').to(controller: :people, action: :show, id: 1, locale: 'pl') }
+    end
+  end
+end
