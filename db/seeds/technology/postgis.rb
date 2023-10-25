5d61f612828f20e4c79fa3fa26577763e0c7a0fe
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-10-25 10:23:28 +0200

    Update technology record for PostGIS
    I added an SVG file with the brand logo. [1]
    Because this version of the file is much better
    than the one we generated, they replace the old logo.
    
    The advantage of this is that the page should load faster
    (embedded SVG) and the logo should not have artifacts on the sides.
    
    [1]: https://postgis.net/

--- db/seeds/technology/postgis.rb
+++ db/seeds/technology/postgis.rb
@@ -1,11 +1,13 @@
 # PostGIS
 # https://postgis.net/
 technology_postgis = Technology.where(slug: 'postgis').first_or_initialize
-icon_postgis_original_svg = File.read('./db/seeds/technology/svg/postgis.svg')
+icon_postgis_brand_svg = File.read('./db/seeds/technology/svg/postgis-brand.svg')
+# icon_postgis_original_svg = File.read('./db/seeds/technology/svg/postgis.svg')
 technology_postgis.assign_attributes(
   name: 'postgis',
   title: 'PostGIS',
-  icon: icon_postgis_original_svg
+  icon: icon_postgis_brand_svg
+  # icon_wordmark: icon_postgis_original_svg
 )
 technology_postgis.save
 technology_postgis.tool!
