7e7c101db5943852a2b0e2b93de046728ac16a3f
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-10-27 11:35:47 +0200

    Fix Bootstrap version in system tests
    We updated the Bootstrap version but forgot about tests.

--- spec/system/check_version_of_javascript_libraries_spec.rb
+++ spec/system/check_version_of_javascript_libraries_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Application loads JavaScript libraries' do
     leaflet_version = browser.evaluate('L.version')
 
     aggregate_failures('verify version of JavaScript libraries') do
-      expect(bootstrap_version).to eq('5.3.1')
+      expect(bootstrap_version).to eq('5.3.2')
       expect(jquery_version).to eq('3.7.1')
       expect(leaflet_version).to eq('1.9.4')
     end
