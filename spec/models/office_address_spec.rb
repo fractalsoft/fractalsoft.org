b4bae1dbe69851db50f93181435ad8d05a8b4f72
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-01 18:43:33 +0100

    Add test to Office Address

--- spec/models/office_address_spec.rb
+++ spec/models/office_address_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe OfficeAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#sorted' do
+    it 'returns office addresses in position order' do
+      second_office_address = create(:office_address, position: 2)
+      third_office_address = create(:office_address, position: 3)
+      first_office_address = create(:office_address, position: 1)
+
+      expect(described_class.sorted).to eq(
+        [
+          first_office_address,
+          second_office_address,
+          third_office_address
+        ]
+      )
+    end
+  end
 end
