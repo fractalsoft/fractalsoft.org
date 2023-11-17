d4aa00c146908f5eb3d3dae17837965789a1096e
Anna Bargiel <ania-hm@users.noreply.github.com>
2023-11-17 15:35:28 +0100

    Change the order of contributions

--- scripts/one_offs/update_profiles_data.rb
+++ scripts/one_offs/update_profiles_data.rb
@@ -5,24 +5,20 @@
 # bundle exec rails runner scripts/one_offs/update_profiles_data.rb
 
 ania = Person.find_by(nickname: 'ania')
-
-selenium = Technology.find_by(slug: 'selenium-webdriver')
-
-# rubocop:disable Rails/SkipsModelValidations
-TechnologicalSkill.upsert_all(
-  [
-    { person_id: ania.id, rating: 10, technology_id: selenium.id }
-  ],
-  unique_by: [:person_id, :technology_id],
-  update_only: [:rating]
-)
-# rubocop:enable Rails/SkipsModelValidations
-
 fractalsoft = Project.find_by(slug: 'fractalsoft.org')
-contribution = Contribution.create_with(name: 'Copywriting')
-                           .find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 9 })
-Globalize.with_locale(:pl) { contribution.update(name: 'Copywriting') }
 
-contribution = Contribution.create_with(name: 'Refactoring the Markdown parser from Redcarpet to CommonMarker')
-                           .find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 10 })
-Globalize.with_locale(:pl) { contribution.update(name: 'Refaktoring parsera Markdowna z Redcarpet na CommonMarker') }
+contribution = Contribution.find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 7 })
+Globalize.with_locale(:en) { contribution.update(name: 'Markdown parser (Redcarpet → CommonMarker)') }
+Globalize.with_locale(:pl) { contribution.update(name: 'Parser Markdowna (Redcarpet → CommonMarker)') }
+
+contribution = Contribution.find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 8 })
+Globalize.with_locale(:en) { contribution.update(name: 'Importmaps optimization') }
+Globalize.with_locale(:pl) { contribution.update(name: 'Optymalizacja importmaps') }
+
+contribution = Contribution.find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 9 })
+Globalize.with_locale(:en) { contribution.update(name: 'Writing unit tests') }
+Globalize.with_locale(:pl) { contribution.update(name: 'Pisanie testów jednostkowych') }
+
+contribution = Contribution.find_or_create_by({ person_id: ania.id, project_id: fractalsoft.id, position: 10 })
+Globalize.with_locale(:en) { contribution.update(name: 'Copywriting') }
+Globalize.with_locale(:pl) { contribution.update(name: 'Copywriting') }
