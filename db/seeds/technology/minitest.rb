bed0d71fa9345391de09b7c24d75eb36fe596c6e
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-20 10:32:02 +0100

    Add an SVG image for the minitest technology
    Our minitest record had no image.
    This commit provides an image [1]
    that I converted from PNG format with Adobe Express tool [2]
    and then enhanced with Inkscape. [3]
    Finally, of course, I optimized the SVG file. [4]
    
    [1]: https://github.com/metaskills/minitest-spec-rails
    [2]: https://new.express.adobe.com/tools/convert-to-svg
    [3]: https://inkscape.org/
    [4]: https://github.com/svg/svgo

--- db/seeds/technology/minitest.rb
+++ db/seeds/technology/minitest.rb
@@ -1,7 +1,9 @@
 # minitest
 technology_minitest = Technology.where(slug: 'minitest').first_or_initialize
+icon_minitest_svg = File.read('./db/seeds/technology/svg/minitest-spec-rails.svg')
 technology_minitest.assign_attributes(
   github_repo: 'minitest/minitest',
+  icon: icon_minitest_svg,
   name: 'minitest',
   title: 'minitest',
   website_url: 'https://docs.seattlerb.org/minitest/'
