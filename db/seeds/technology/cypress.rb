c72c2a26d3c16df069a2d5fadffba0bfb1d36ea7
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-21 00:58:05 +0100

    Add more emojis to technology records
    With the help of ChatGPT,
    I chose appropriate emojis for various technologies.
    Some of ChatGPT's suggestions were wrong in my opinion,
    so I left them out.

--- db/seeds/technology/cypress.rb
+++ db/seeds/technology/cypress.rb
@@ -2,10 +2,11 @@
 technology_cypress = Technology.where(slug: 'cypress').first_or_initialize
 icon_cypress_original_svg = File.read('./db/seeds/technology/svg/cypress.svg')
 technology_cypress.assign_attributes(
+  emoji: '🌲👁️‍🗨️🚀',
   github_repo: 'cypress-io/cypress',
+  icon: icon_cypress_original_svg,
   name: 'cypress',
   title: 'Cypress',
-  icon: icon_cypress_original_svg,
   website_url: 'https://www.cypress.io/'
 )
 technology_cypress.save
