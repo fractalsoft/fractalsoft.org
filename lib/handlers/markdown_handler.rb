1d2a8307502e5b61d6c9d2ac17845a60a65543bb
Anna Bargiel <ania-hm@users.noreply.github.com>
2023-11-20 13:17:31 +0100

    Refactor MarkdownHandler
    The removed part of the code was used with Redcarpet.
    I found an information:
    
    > The call method needs to return
    > a string containing the code that needs to be executed
    > by Rails to generate HTML (in this template handler,
    > because of the self.default_format = Mime[:html] line).
    > (There isn’t much, if any, documentation on this
    > –I got this from the articles I found online.) [1]
    
    I removed the unnecessary part of the code
    because we use another class that returns HTML code.
    
    [1]: https://blog.edruder.com/2017/12/19/add-markdown-to-rails-5.html

--- lib/handlers/markdown_handler.rb
+++ lib/handlers/markdown_handler.rb
@@ -2,9 +2,6 @@ require 'markdown'
 
 module Handlers
   class MarkdownHandler
-    class_attribute :default_format
-    self.default_format = Mime[:html]
-
     def call(_template, body)
       Markdown.new(body).to_html.inspect
     end
