6d7dee1228fa25c4bc3dc84940a29ea8b1c974a8
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-22 23:44:46 +0100

    Remove leaflet version check from system test
    > Leaflet is the leading open-source JavaScript library [1]
    > for mobile-friendly interactive maps.
    
    [1]: https://leafletjs.com/

--- spec/system/check_version_of_javascript_libraries_spec.rb
+++ spec/system/check_version_of_javascript_libraries_spec.rb
@@ -7,12 +7,10 @@ RSpec.describe 'Application loads JavaScript libraries' do
     browser = page.driver.browser
     bootstrap_version = browser.evaluate('bootstrap.Tooltip.VERSION')
     jquery_version = browser.evaluate('jQuery().jquery')
-    leaflet_version = browser.evaluate('L.version')
 
     aggregate_failures('verify version of JavaScript libraries') do
       expect(bootstrap_version).to eq('5.3.2')
       expect(jquery_version).to eq('3.7.1')
-      expect(leaflet_version).to eq('1.9.4')
     end
   end
 end
