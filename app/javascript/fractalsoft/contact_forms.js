23e2d4d95fe90b43deaa0e5a3b0db79f232afb0d
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-22 23:30:43 +0100

    Change jQuery code to Vanilla JS code

--- app/javascript/fractalsoft/contact_forms.js
+++ app/javascript/fractalsoft/contact_forms.js
@@ -1,11 +1,23 @@
 document.addEventListener("turbolinks:load", function() {
-  $('.btn-map').click(function() {
-    const target = '#' + $(this).data('target');
-    $('.btn-map').removeClass('active');
-    $(this).addClass('active');
-    $('iframe.map').addClass('map-display-none');
-    return $(target).removeClass('map-display-none');
+  var mapButtons = document.querySelectorAll('.btn-map');
+
+  mapButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var targetId = '#' + button.getAttribute('data-target');
+      mapButtons.forEach(function (btn) {
+        btn.classList.remove('active');
+      });
+      button.classList.add('active');
+      document.querySelectorAll('iframe.map').forEach(function (iframe) {
+        iframe.classList.add('map-display-none');
+      });
+      document.querySelector(targetId).classList.remove('map-display-none');
+    });
   });
 
-  return $('#contact_form').ready(() => $('#contact_form_browser_info').val('' + browserInfo.json()));
+  var contactForm = document.getElementById('contact_form');
+  if (contactForm) {
+    var browserInfoInput = document.getElementById('contact_form_browser_info');
+    if (browserInfoInput) { browserInfoInput.value = '' + browserInfo.json(); }
+  }
 });
