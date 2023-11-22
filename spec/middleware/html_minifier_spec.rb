b0d8d769626ca56029c705cd26dc5e8aa83e36f0
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-22 11:17:03 +0100

    Fix HtmlMinifier to handle frozen strings
    On the production server,
    we received the following message in the logs:
    
    ```
    FrozenError - can't modify frozen String: "":
      app/middleware/html_minifier.rb:37:in `block (2 levels) in compress'
      app/middleware/html_minifier.rb:36:in `block in compress'
      app/middleware/html_minifier.rb:35:in `compress'
      app/middleware/html_minifier.rb:26:in `call'
    ```
    
    We didn't see this in development mode because we only enable
    the `HtmlMinifier` middleware in production.
    Additionally, the problem only occurred during redirects.
    
    To fix this, I added unit tests for the middleware [1]
    and reproduced the case where the response contains a frozen string.
    
    The fix skips the chunk that is frozen.
    And the tests will help us protect ourselves
    from a similar error in the future.
    
    [1]: https://stackoverflow.com/questions/17506567/testing-middleware-with-rspec

---
+++ spec/middleware/html_minifier_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe HtmlMinifier do
+  context 'when rendering any HTML view' do
+    let(:body) do
+      [
+        "<p>join</p>\n<p>lines</p>",
+        '<p>unnecessary</p>    <p>spaces</p>',
+        '<p><!-- unnecessary comment -->important content</p>',
+        '<script>function log() { var e = 2.7; console.log(e); }</script>'
+      ]
+    end
+    let(:expected) do
+      [
+        '<p>join</p><p>lines</p>',
+        '<p>unnecessary</p><p>spaces</p>',
+        '<p>important content</p>',
+        '<script>function log() {var e = 2.7;console.log(e);}</script>'
+      ]
+    end
+
+    it 'returns minified HTML without changing the content' do
+      app = ->(_) { [200, { 'Content-Type' => 'text/html' }, body] }
+      html_minifier = described_class.new(app)
+
+      expect(html_minifier.call({}).last).to eq(expected)
+    end
+  end
+
+  context 'when redirecting' do
+    let(:body) { [''.freeze] }
+    let(:expected) { body }
+
+    it 'returns the empty chunk unchanged' do
+      app = ->(_) { [302, { 'Content-Type' => 'text/html' }, body] }
+      html_minifier = described_class.new(app)
+
+      expect(html_minifier.call({}).last).to eq(expected)
+    end
+  end
+
+  context 'when rendering non-HTML view' do
+    let(:body) { ["lorem\nipsum"] }
+    let(:expected) { body }
+
+    it 'returns the original content unchanged' do
+      app = ->(_) { [200, { 'Content-Type' => 'text/plain' }, body] }
+      html_minifier = described_class.new(app)
+
+      expect(html_minifier.call({}).last).to eq(expected)
+    end
+  end
+end
