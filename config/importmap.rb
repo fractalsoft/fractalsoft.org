5be5566b785fb29d978ec2d63b54a51781990e3f
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-23 09:36:51 +0100

    Do not load jQuery library
    We don't use jQuery anymore. [1]
    Our frontend code is pure JavaScript.
    Even Bootstrap does not require jQuery.
    The jQuery is optional in Bootstrap. [2]
    Therefore, we can safely stop using this library.
    
    [1]: https://jquery.com/
    [2]: https://getbootstrap.com/docs/5.3/getting-started/javascript/#optionally-using-jquery

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,6 @@
 
 pin "application", preload: true
 pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet-src.js"
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
