f1469d88f1a3182fc0f081a7cc513e12d41f5cc2
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2023-11-23 09:45:24 +0100

    Do not load Leaflet library
    We are not currently using Leaflet on our website. [1]
    We got what we wanted to achieve in plain HTML using OpenStreetMap. [2]
    If we want to redesign and beautify it in the future,
    we will use Leaflet.
    
    We can be inspired by examples from the official Leaflet.js website. [3]
    We can use our logo to mark the office on the map
    or use geolocation to calculate how far it is from our office.
    We really have a lot of ideas.
    
    [1]: https://leafletjs.com/
    [2]: https://www.openstreetmap.org/
    [3]: https://leafletjs.com/examples.html

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet-src.js"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
