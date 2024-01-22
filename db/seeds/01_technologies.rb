fd0b07ae586ea70a76d2a067e15b5ef274aea1d4
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2024-01-22 22:00:01 +0100

    Create technology record for Go
    > Go is an open source programming language that makes it easy
    > to build simple, reliable, and efficient software.
    
    > An open-source programming language supported by Google [1]
    
    [1]: https://go.dev/

--- db/seeds/01_technologies.rb
+++ db/seeds/01_technologies.rb
@@ -11,6 +11,7 @@ require_relative 'technology/sinatra'
 require_relative 'technology/rails_event_store'
 ## programming language
 require_relative 'technology/csharp'
+require_relative 'technology/go'
 require_relative 'technology/jruby'
 require_relative 'technology/python'
 require_relative 'technology/ruby'
