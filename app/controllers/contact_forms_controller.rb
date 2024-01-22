2f955ad9e388db7006d90307542ccb3a8864a3f6
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2024-01-22 21:31:57 +0100

    Change for linter purposes
    Rubocop detected a problem:
    
    > Metrics/AbcSize:
    > Assignment Branch Condition size for create is too high.
    > [<5, 22, 2> 22.65/17]
    
    So I refactored that fixes it.

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 class ContactFormsController < ApplicationController
+  before_action :set_remote_ip
+
   def new
     @contact_form = ContactForm.new
   end
 
   def create
-    request.remote_ip = request.env['HTTP_FLY_CLIENT_IP'] || request.remote_ip
     @contact_form = ContactForm.new(params[:contact_form])
     @contact_form.request = request
     @contact_form.deliver ? flash[:notice] = t('.success') : flash[:alert] = t('.fail')
     redirect_to "#{@contact_form.request.referrer}#contact"
+  end
+
+  private
+
+  def set_remote_ip
+    request.remote_ip = request.env['HTTP_FLY_CLIENT_IP'] || request.remote_ip
   end
 end
