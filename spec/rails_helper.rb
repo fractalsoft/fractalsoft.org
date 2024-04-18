7333ee29d81e6544253c258d47cb12fc75f48a09
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2024-04-18 13:46:02 +0200

    Fix deprecation warning about fixture_path
    > Rails 7.1 has deprecated the singular fixture_path in favour
    > of an array. You should migrate to plural:
    
    I will make the change as suggested.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.fixture_paths = Rails.root.join('spec/fixtures')
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
 end
