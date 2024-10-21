16dcab9c69beb499198d3ce6011965d89ba2ac9d
Grzegorz Lisowski <glisowski@fractalsoft.org>
2024-10-21 17:02:38 +0200

    Update intl-tel-input
    To keep things up to date we want to update dependencies from our
    importmaps. Intl tel input was out of date so we want to update it.
    We run `bin/importmap update` which bumps the js version and puts the
    current version of the package into vendor. We also update css to match
    the version and remove the css hacks that were but are no longer necesary
    with the newer version.

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,6 @@
 pin "application", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "intl-tel-input", to: "https://ga.jspm.io/npm:intl-tel-input@18.3.3/index.js", preload: true
+pin "intl-tel-input" # @24.6.0
 pin "turbolinks", to: "https://ga.jspm.io/npm:turbolinks@5.2.0/dist/turbolinks.js"
 pin_all_from "app/javascript/fractalsoft", under: "fractalsoft"
