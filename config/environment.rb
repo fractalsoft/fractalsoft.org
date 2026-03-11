7b3010310716f568665776e9f3ccd59a16fa9c9a
Grzegorz Lisowski <glisowski@fractalsoft.org>
2026-03-11 17:09:51 +0100

    feat: rails.application.secrets is deprecated

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,12 @@ Rails.application.initialize!
 class Rails::Application
   def secret_key_base
     if Rails.env.development? || Rails.env.test? || ENV["SECRET_KEY_BASE_DUMMY"]
-      secrets.secret_key_base ||= generate_development_secret
+      ENV["SECRET_KEY_BASE"].presence ||
+        credentials.secret_key_base.presence ||
+        generate_development_secret
     else
       validate_secret_key_base(
-        ENV["SECRET_KEY_BASE"] || credentials.secret_key_base || secrets.secret_key_base
+        ENV["SECRET_KEY_BASE"] || credentials.secret_key_base
       )
     end
   end
