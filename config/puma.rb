c5ca4a7b1283bf030a4e6f1d47d3b4af257d8bf2
Grzegorz Lisowski <glisowski@fractalsoft.org>
2026-03-19 10:14:22 +0100

    refactor(ui): polish home and people page styling

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,11 @@
-rails_env = ENV.fetch('RAILS_ENV') { 'development' }
-
-# In development, prefer single-mode to avoid stale asset digests when using Propshaft
-# with preloaded, multi-worker Puma.
-workers ENV.fetch('WEB_CONCURRENCY') { rails_env == 'development' ? 0 : 2 }
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
-preload_app! unless rails_env == 'development'
+preload_app!
 
 port        ENV.fetch('PORT') { 3000 }
-environment rails_env
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 before_fork do
   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
